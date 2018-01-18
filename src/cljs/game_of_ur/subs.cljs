605b94cf5e737045b2e837be76118bca2a7792c1
Camilo Polymeris <cpolymeris@gmail.com>
2018-01-17 19:35:38 -0600

    Spec'd subs

--- src/cljs/game_of_ur/subs.cljs
+++ src/cljs/game_of_ur/subs.cljs
@@ -1,14 +1,16 @@
 (ns game-of-ur.subs
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :as re-frame]
-            [game-of-ur.game.board :as board]))
+            [game-of-ur.game.board :as board]
+            [cljs.spec.alpha :as spec]))
 
 (re-frame/reg-sub
   :board-state
   (fn [db]
-    (reduce board/child-board board/initial-board (:moves db))))
+    (spec/assert ::board/board (reduce board/child-board board/initial-board (:moves db)))))
 
 (re-frame/reg-sub
   :last-move
   (fn [db]
-    (last (:moves db))))
+    (when-not (empty? (:moves db))
+      (spec/assert ::board/full-move (last (:moves db))))))
