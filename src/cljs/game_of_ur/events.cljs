c91948b247d3c2d866878af47d984037c67d5ae0
Camilo Polymeris <cpolymeris@gmail.com>
2018-01-27 16:57:09 -0600

    Play stones

--- src/cljs/game_of_ur/events.cljs
+++ src/cljs/game_of_ur/events.cljs
@@ -1,6 +1,7 @@
 (ns game-of-ur.events
   (:require [re-frame.core :as re-frame]
-            [game-of-ur.db :as db]))
+            [game-of-ur.db :as db]
+            [game-of-ur.game.board :as board]))
 
 (re-frame/reg-event-db
   :initialize-db
@@ -18,3 +19,18 @@
   :roll-dice
   (fn [db _]
     (assoc db :roll (rand-nth [0 4 1 1 1 1 3 3 3 3 2 2 2 2 2 2]))))
+
+(re-frame/reg-event-db
+  :play-stone
+  (fn [db [_ coords]]
+    (if-let [roll (:roll db)]
+      (let [board-state (reduce board/child-board board/initial-board (:moves db))
+            move (board/full-move {:roll   roll
+                                   :player (:turn board-state)
+                                   :origin coords})]
+        (if (board/valid-move? board-state move)
+          (-> db
+              (update :moves conj move)
+              (dissoc :roll))
+          db))
+      db)))
